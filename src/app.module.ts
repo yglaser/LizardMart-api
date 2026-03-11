f68d644c1f2e03738b95cd6134789401637d6fc4
yanina glaser <yaninaglaser@gmail.com>
2026-03-11 14:44:44 -0300

    proyect init architecture

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, UsersModule, PrismaModule],
 })
 export class AppModule {}
